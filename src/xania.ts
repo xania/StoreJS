46b8cc916ec58ff2925ef1c28ba2055302f3f19b
xania <ibrahim.bensalah@gmail.com>
2018-10-21 17:11:05 +0200

    default XaniaViewResult

--- src/xania.ts
+++ src/xania.ts
@@ -2,8 +2,20 @@ import { renderAll, ITemplate } from "./xania/index.js"
 export { tpl } from "./xania/elements/index.js"
 import { DomDriver } from "./xania/binding.js"
 
-export default { 
-    render(dom : HTMLElement, template: ITemplate) {
-        return renderAll(new DomDriver(dom), template);
+export function renderer(target: HTMLElement) {
+    return (value: XaniaViewResult) => {
+        return value.render({ target });
     }
 }
+
+export default function (template: ITemplate): XaniaViewResult {
+    return {
+        render({ target }) {
+            return renderAll(new DomDriver(target), template);
+        }
+    } as XaniaViewResult
+}
+
+interface XaniaViewResult {
+    render(context: { target: HTMLElement }): { dispose(); }
+};
